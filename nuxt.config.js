9ee5137367aa60743c94f4ccc5086a23fea2f045
Aleksandr <kiselev.alexander.fl@gmail.com>
2018-11-20 09:33:09 +0300

    'init'

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,9 @@
 module.exports = {
     plugins: [
-        // Adds internationalization and global components
         '~/plugins/element-ui.js'
     ],
     css: [
         'element-ui/lib/theme-chalk/index.css',
         '~/css/index.css'
     ]
-  }
+}
