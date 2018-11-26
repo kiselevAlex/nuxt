b216d62d0d475919e4a1ff66b435b289cf32159f
Alexander <kiselev.alexander.fl@gmail.com>
2018-11-26 21:32:50 +0300

    b

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,5 +5,13 @@ module.exports = {
     css: [
         'element-ui/lib/theme-chalk/index.css',
         '~/css/index.css'
-    ]
+    ],
+    head: {
+        titleTemplate: 'Рейтинг франшиз',
+        meta: [
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          { hid: 'description', name: 'description', content: 'Рейтинг франшиз на портале БиБосс.ру' }
+        ]
+    }
 }
