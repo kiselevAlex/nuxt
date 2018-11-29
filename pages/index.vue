470aaa94318df1389f354c7c38de869dbaa8a261
Aleksandr <kiselev.alexander.fl@gmail.com>
2018-11-29 16:34:24 +0300

    links

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="w-full">
     <div class="raiting">
-      <h1 class="raiting__title">Pейтинг франшиз 2018</h1>
+      <h1 class="raiting__title">Pейтинг франшиз {{year}}</h1>
+      <div class="f-l">
+          <span class="m-r20"><nuxt-link active-class='active' :to="'/spa'">CSR</nuxt-link></span>
+      </div>
+      <div class="t-c">
+          <span class="m-r20" v-for="y in years" :key="y"><nuxt-link active-class='active' :to="'/' + y">{{y}}</nuxt-link></span>
+      </div>
       <div>
         <div v-for="el in raiting" :key="el.id" class="item">
           <div class="item__logo">
@@ -21,10 +27,18 @@
 
 export default {
   middleware: ['init'],
+  data() {
+      return {
+          years: ['2018','2017','2016','2015','2014']
+      }
+  },
   computed: {
     raiting(){
       return this.$store.state.raiting;
-    }
+    },
+    year(){
+        return this.$route.params.year;
+    },
   }
 }
 
--- pages/spa.vue
+++ pages/spa.vue
@@ -1,18 +1,25 @@
 <template>
     <div class="w-full">
+
         <preloader v-if="preloader" />
         <div v-else class="raiting">
-            <h1 class="raiting__title">Pейтинг франшиз 2018</h1>
-            <div>
-            <div v-for="el in raiting" :key="el.id" class="item">
-                <div class="item__logo">
-                <img :src="el.logo"/>
-                </div>
-                <div class="item__about">
-                <div class="item__title">{{el.name}}</div>
-                <div class="item__description">{{el.description}}</div>
-                </div>
+            <h1 class="raiting__title">Pейтинг франшиз {{year}}</h1>
+            <div class="f-l">
+                <span class="m-r20"><nuxt-link :to="'/'">SSR</nuxt-link></span>
             </div>
+            <div class="t-c">
+                <span class="m-r20" v-for="y in years" :key="y"><nuxt-link active-class='active' :to="'/spa/' + y">{{y}}</nuxt-link></span>
+            </div>
+            <div>
+                <div v-for="el in raiting" :key="el.id" class="item">
+                    <div class="item__logo">
+                    <img :src="el.logo"/>
+                    </div>
+                    <div class="item__about">
+                    <div class="item__title">{{el.name}}</div>
+                    <div class="item__description">{{el.description}}</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -20,10 +27,19 @@
 
 <script>
 import preloader from '@/components/lib/preloader';
-
 export default {
+    data() {
+        return {
+            years: ['2018','2017','2016','2015','2014']
+        }
+    },
     components: {
         preloader
+    },
+    watch: {
+        '$route'(){
+            this.$store.dispatch('raiting', {year: this.$route.params.year || (new Date()).getFullYear()});
+        }
     },
     created(){
         this.$store.dispatch('raiting', {year: this.$route.params.year || (new Date()).getFullYear()});
@@ -34,8 +50,10 @@ export default {
         },
         preloader(){
             return this.$store.state.preloader;
-        }
+        },
+        year(){
+            return this.$route.params.year;
+        },
     }
 }
-
 </script>
